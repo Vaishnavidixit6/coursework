83cdf5f88584010a295da60f496fd8d6cff02200
Vaishnavi <vaishnavidixit@mail.adelphi.edu>
2025-06-06 10:32:47 -0400

    This is my Day 8 assignment

--- week2/Day7.R
+++ week2/Day7.R
@@ -79,11 +79,72 @@ sum(ex2$group == 'HIGH') / nrow(ex2)
 sum(pop2$group == 'HIGH') / nrow(pop2)
 
 # Q3
-estimate_coin_bias <- function(n, p) {
-  mean(rbinom(n,1,p))
+P_hat = rep(0,10^5)
+for (i in 1:10^5){
+  X <- sample(pop2$group, 150, replace = TRUE)
+  X
+  P_hat[i] <- sum(X == 'HIGH') / length(X)
 }
+P <- sum(P_hat) / length(P_hat)
+P
 
-n <- nrow(ex2)
-p <- propor
-p_hat <- replicate(1e5, estimate_coin_bias(n, p))
+# Q4
+var(P_hat)
 
+# Q5
+
+(P*(1-P)) / 150
+
+# 2.2
+# a
+45/69
+30/34
+# b
+# H_0: p_C = p_T
+# H_a: p_C > p_T
+# We write alive on 27 cards representing patients who were alive at
+# the end of the study, and dead on 75 cards representing patients
+# who were not. Then, we shuffle these cards and split them into two groups:
+# one group of size 69 representing treatment, and another group of
+# size 34 representing control. We calculate the difference between
+# the proportion of dead cards in the treatment and control groups (treatment -
+# control) and record this value. We repeat this many times to build a distribution
+# centered at p_C - p_T = 0. Lastly, we calculate the fraction of simulations where
+# the simulated differences in proportions are p_C - p_T greater than 0. If this fraction is low,
+# we conclude that it is unlikely to have observed such an outcome by chance and
+# that the null hypothesis should be rejected in favor of the alternative.
+
+# c
+41/99 
+# Since the p-value is so big we fail to reject null hypothesis
+# because we don't have enough evidence to reject the null hypothesis, treatment is not effective
+
+# 2.6
+# Hypothesis
+# H_0: p_T = p_C
+# H_a: p_C < p_T
+
+a <- 10/34
+b <- 4/16
+a-b
+
+0.4955 / 1.0
+# Since the p-value is so big we fail to reject null hypothesis
+# because we don't have enough evidence to reject the null hypothesis, yawning is not contagious
+
+# 3.1
+# (a) The distribution of the sample proportions of vegetarians in random samples of size 60 is
+# approximately normal since n ≥ 30. 
+# False because, n>=30,  np>=10 which in this case is 4.8 !>=10, n(1-p) >=10
+# (b) The distribution of the sample proportions of vegetarian college students in random samples
+# of size 50 is right skewed.
+# 
+# (c) A random sample of 125 college students where 12% are vegetarians would be considered
+# unusual.
+# 
+# (d) A random sample of 250 college students where 12% are vegetarians would be considered
+# unusual.
+# 
+# (e) The standard error would be reduced by one-half if we increased the sample size from 125
+# to 250.
+# 
