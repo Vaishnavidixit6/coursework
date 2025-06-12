553f4ee4a984c626f18e8708cba6a7b7ab9d70b5
Vaishnavi <vaishnavidixit@mail.adelphi.edu>
2025-06-12 13:20:47 -0400

    This is Day 1 for week 3

--- week3/Day1_Week3.R
+++ week3/Day1_Week3.R
@@ -53,7 +53,7 @@ contrasts(ShelveLoc) # shows how the dummy is using R code for the diff categori
 # a)
 # y-cap = 123.05 - 8.94x 
 # b)
-# if the mother smokes then the babies weight decreases by 8.94 units
+# if the mother smokes then the average birth weight of baby decreases by 8.94 units
 123.05 - (8.94*1) # smoker
 123.05 # non-smoker
 
@@ -72,4 +72,35 @@ contrasts(ShelveLoc) # shows how the dummy is using R code for the diff categori
 # c)
 # since, the p-value is more than 0.05, we can say that parity and average birth wt does not have a statistically sig relationship
 
+# 6.3
+# a)
+# y-cap = -80.41 + 0.44 beta_1 -3.33 beta_2 -0.01 beta_3 + 1.15 beta_4 + 0.05 beta_5 -8.40 beta_6
 
+# b)
+# For every unit change in gestation, the average weight of the baby increases by 0.44 units keeping all the other variables constant
+# For every unit change in age, the avg weight of the baby decreases by 0.01 units keeping all th eother variables constant
+
+# c) The estimate is different in 6.2 and 6.3 for parity as there can be the effect of a confounding variable which is an unknown factor that can affect the dependent and independent varaibles
+# there can also be a possibility of multicol
+
+# d) 
+-80.41 + 0.44*284 -3.33*0 -0.01*27 + 1.15*62 + 0.05*100 -8.40*0
+# Residual = 120 - 120.58 = -0.58
+
+# e) R^2 = 1 - 249.28 / 332.57
+# 1- 249.28 / 332.57 * (1236 - 1)/ (1236 - 6 - 1) 
+
+# 6.1
+library(readr)
+baby <- read.table("week3/babyweights.txt")
+View(baby)
+lm.fit1 = lm(bwt~smoke,data=baby )
+summary(lm.fit1)
+
+# 6.2
+lm.fit1 = lm(bwt~parity,data=baby )
+summary(lm.fit1)
+
+# 6.3
+lm.fit1 = lm(bwt~.,data=baby )
+summary(lm.fit1)
